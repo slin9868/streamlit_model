4a737306a3834e69e79a3d35b904232d104c1a3e
slin9868 <151382035+slin9868@users.noreply.github.com>
2023-12-09 20:38:22 -0500

    Update streamlit_pickle.py

--- streamlit_pickle.py
+++ streamlit_pickle.py
@@ -3,6 +3,15 @@ import streamlit as st
 import numpy as np
 import pickle
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import ConfusionMatrixDisplay 
+from sklearn.compose import make_column_transformer 
+from sklearn.pipeline import Pipeline
+
 st.header('A Model for AHD')
 
 st.write('Please enter the Age, Sex, and Slope information below.')
